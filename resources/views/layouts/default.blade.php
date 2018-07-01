deda3d74017fa51cfe1f01e4ddfedba20241ff7d
vagrant <vagrant@homestead>
2018-07-01 15:19:37 +0000

    layouts

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample') App</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
-    <br>
-    <a href="{{route('home')}}">首页</a>
-    <br>
-    <a href="{{route('about')}}">关于</a>
-    <br>
-    <a href="{{route('help')}}">帮助</a>
+    <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <div class="container">
+      @yield('content')
+    </div>    
   </body>
 </html>
